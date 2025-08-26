bffd06186c066bb9af2bf8bc588ff24f211fb9a9
matheusbertolucio20 <matheus.bertolucio@fatec.sp.gov.br>
2025-08-26 11:36:13 -0300

    Adicionando pasta product ao repositório principal

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Product
 
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.1.6.
+This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.1.5.
 
 ## Development server
 
--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Product } from './product/product';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Product
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
